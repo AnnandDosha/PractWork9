e3f6098189aa53143560a8b401f1f0579dd5e320
AnnandDosha <anna2000gold@mail.ru>
2020-06-22 20:40:31 +0300

    PR_9

---
+++ PR_9/MainForm.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace PR_9
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.ballLabel = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.racket = new System.Windows.Forms.Panel();
+            this.ball = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Location = new System.Drawing.Point(25, 10);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(41, 14);
+            this.scoreLabel.TabIndex = 0;
+            this.scoreLabel.Text = "Счёт: 0";
+            // 
+            // ballLabel
+            // 
+            this.ballLabel.AutoSize = true;
+            this.ballLabel.Location = new System.Drawing.Point(493, 10);
+            this.ballLabel.Name = "ballLabel";
+            this.ballLabel.Size = new System.Drawing.Size(101, 14);
+            this.ballLabel.TabIndex = 1;
+            this.ballLabel.Text = "Потеряно мячей: 0";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 10;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // racket
+            // 
+            this.racket.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.racket.Font = new System.Drawing.Font("Mongolian Baiti", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.racket.Location = new System.Drawing.Point(207, 306);
+            this.racket.Name = "racket";
+            this.racket.Size = new System.Drawing.Size(188, 26);
+            this.racket.TabIndex = 2;
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ball.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ball.Location = new System.Drawing.Point(281, 227);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(36, 40);
+            this.ball.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(288, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 14);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Пауза";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Visible = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(628, 335);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.racket);
+            this.Controls.Add(this.ballLabel);
+            this.Controls.Add(this.scoreLabel);
+            this.Font = new System.Drawing.Font("Modern No. 20", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Coral;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Арканоид";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Label ballLabel;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Panel racket;
+        private System.Windows.Forms.Panel ball;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ PR_9/MainForm.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PR_9
+{
+    public partial class MainForm : Form
+    {
+        public int ballSpeedX;//поле отображающее скорость по Х
+        public int ballSpeedY;//поле отображающее скорость по У
+        public int lostball;//переменна для условия при потори мяча
+        public int score=0;// переменная при подсчёте очков 
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        { // изменение контура панели мяча на круглую
+            System.Drawing.Drawing2D.GraphicsPath path = new System.Drawing.Drawing2D.GraphicsPath();
+            path.AddEllipse(ball.ClientRectangle);
+            ball.Region = new Region(path);
+            //код размещающий на форме панели-кирпичи и их цвет
+            int cellSize = 30;
+            for (int x = 0; x < 10; x++)
+            {
+                for (int y = 0; y < 4; y++)
+                {
+                    Panel cell = new Panel();
+                    cell.Width = this.Left/10;
+                    cell.Height = cellSize;
+                    cell.Location = new Point(x * cell.Size.Width, y * cellSize);
+                    cell.BorderStyle = BorderStyle.FixedSingle;
+                    if(y==0)
+                    {
+                        cell.BackColor = Color.DarkRed;
+                    }
+                    if (y == 1)
+                    {
+                        cell.BackColor = Color.Orange;
+                    }
+                    if (y == 2)
+                    {
+                        cell.BackColor = Color.YellowGreen;
+                    }
+                    if (y == 3)
+                    {
+                        cell.BackColor = Color.DarkGreen;
+                    }
+                    this.Controls.Add(cell);
+                }
+            }
+            //код изменяющий положение мяча и его скорость
+            Random random = new Random();
+            Point randompoint = new Point(random.Next(this.Width),random.Next(this.Height/4)+this.Height/2 - ball.Height);
+            ball.Location = randompoint;
+            ballSpeedX = random.Next(6) - 3;
+            ballSpeedY = -(random.Next(2) + 2);
+        }
+        //обработчик движения мыши
+        private void MainForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            int racketCenterX = racket.Width / 2;
+            if(e.X > racketCenterX && e.X < this.ClientSize.Width - racketCenterX)
+            {
+                racket.Location = new Point(e.X - racketCenterX, racket.Top);
+            }
+        }
+        //код реализующий отскакивание мяча от стенок 
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            if(ball.Left<=0)
+            {
+                ballSpeedX = -ballSpeedX;
+            }
+            if (ball.Right >= this.ClientSize.Width)
+            {
+                ballSpeedX = -ballSpeedX;
+            }
+            if (ball.Top <= 0)
+            {
+                ballSpeedY = -ballSpeedY;
+            }
+            //код реализующий отскакивание мяча от ракетки
+            int X = (ball.Right - ball.Left)/2 + ball.Left;
+            int Y = ball.Bottom;
+            if(Y>=racket.Top && X>racket.Left && X<racket.Right)
+            {
+                ball.Top = racket.Top - ball.Size.Height + 1;
+                ballSpeedY = -ballSpeedY;
+
+            }
+            if (ball.Bottom >= this.ClientSize.Height)
+            {
+                gameTimer.Stop();
+                lostball++;
+                ballLabel.Text = "Потеряно мячей: "+lostball.ToString();
+                MessageBox.Show("Мяч потерян");
+                Random random = new Random();
+                Point randompoint = new Point(random.Next(this.Width), random.Next(this.Height / 4) + this.Height / 2 - ball.Height);
+                ball.Location = randompoint;
+                ballSpeedX = random.Next(6) - 3;
+                ballSpeedY = -(random.Next(2) + 2);
+                gameTimer.Start();
+            }
+            //код отображающий счёт,увеличение очков в зависимомти от ряда кирпичей и отскакивания мяча от них
+            Rectangle ballRectangle = ball.Bounds;
+            for(int i = this.Controls.Count -1; i>= 0; i--)
+            {
+                Control item = this.Controls[i];
+                if(item is Panel && item != ball && item != racket)
+                {
+                    if(ballRectangle.IntersectsWith(item.Bounds))
+                    {
+                        ballRectangle.Intersect(item.Bounds);
+
+                        if(ballRectangle.Height<ballRectangle.Width)
+                        {
+                            ballSpeedY = -ballSpeedY;
+                        }
+                        else
+                        {
+                            ballSpeedX = -ballSpeedX;
+                        }
+                        this.Controls.Remove(item);
+                        score += 100;
+                        scoreLabel.Text = "Счёт: "+score.ToString();
+                    }
+                }
+            }
+            int panelcount=0;
+            for (int i = this.Controls.Count - 1; i >= 0; i--)
+            {
+                Control item = this.Controls[i];
+                if (item is Panel && item != ball && item != racket)
+                {
+                    panelcount++;
+                }
+            }
+            if(panelcount==0)
+            {
+                gameTimer.Stop();
+                MessageBox.Show("Вы победили!");
+            }
+            ball.Location = new Point(ball.Location.X+ballSpeedX, ball.Location.Y + ballSpeedY);
+        }
+        // код для отображения на форме об остановке игры и её продолжении
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode==Keys.Space)
+            {
+               if(gameTimer.Enabled==true)
+               {
+                    gameTimer.Stop();
+                    label2.Visible = true;
+               }
+                else
+                {
+                    gameTimer.Start();
+                    label2.Visible = false;
+                }
+            }
+        }
+    }
+}
